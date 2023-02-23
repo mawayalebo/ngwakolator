69d1eb512756d442b6ab16a2b8d2354a27dcc845
unknown <mawayalebo@gmail.com>
2023-02-23 21:06:25 +0200

    inserting new logo to Landing Comp

--- comps/Landing.js
+++ comps/Landing.js
@@ -4,15 +4,15 @@ const Landing = () => {
     const [year, setYear] = useState(new Date().getFullYear());
     const copyright = `© copyright ${year}`;
     return ( 
-        <div className="flex flex-col justify-center bg-black h-screen w-screen">
+        <div className="flex flex-col justify-center bg-slate-100 h-screen w-screen">
             
             
             <div className="top-100 flex justify-center">
-                <Image src='/logo.png' width={80} height={80}/>
+                <Image src={"/ngwako technical enterprises logo.png"} width={350} height={150}/>
             </div> 
             <div className="flex flex-col mt-5">
-                <h2 className="text-white text-center">a Lebogang Mawaya build</h2>
-                <p className="text-white text-center"> {copyright} </p>
+                <h2 className="text-black text-center">a Lebogang Mawaya build</h2>
+                <p className="text-black text-center"> {copyright} </p>
             </div>
             
                         
