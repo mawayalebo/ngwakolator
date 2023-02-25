cef7bb2751cb9d4ba0078a59b6683ff8b8b42da7
unknown <mawayalebo@gmail.com>
2023-02-25 12:52:38 +0200

    restricting invoice to be visible on page refresh

--- pages/invoice.js
+++ pages/invoice.js
@@ -2,15 +2,31 @@ import InvoiceBody from '../comps/InvoiceBody';
 import InvoiceFooter from '../comps/InvoiceFooter';
 import InvoiceHead from '../comps/InvoiceHead';
 import InvoiceStamp from '../comps/InvoiceStamp';
+import Landing from '../comps/Landing';
+import { selectJobs } from '../app/slices/job.slice';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 
 const Invoice = ()=>{
-    return ( 
+    const router = useRouter();
+    const jobs = useSelector(selectJobs);
+
+    
+    return (
+        <>
+        {
+            jobs.length < 1 && router.push("/") && <Landing/>
+        }
+        {
+        jobs.length > 0 &&
         <div className="max-w-xl mx-auto">
             <InvoiceHead/>
             <InvoiceBody/>
             <InvoiceStamp/>
             <InvoiceFooter/>
         </div>
+        }
+        </>
      );
 }
  
